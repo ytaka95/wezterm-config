7907d3ec01a5edc5623b6f3a968d0ef16e2328fe
Yosuke Takada <31610998+ytaka95@users.noreply.github.com>
2024-10-05 15:21:04 +0900

    update tab bar style, use light modus theme

---
+++ tabbarstyle.lua
@@ -0,0 +1,42 @@
+local wezterm = require 'wezterm'
+
+return {
+   style = {
+      tab_bar = {
+         -- The color of the strip that goes along the top of the window
+         -- (does not apply when fancy tab bar is in use)
+         background = '#f0f0f0',
+         -- The active tab is the one that has focus in the window
+         active_tab = {
+            -- The color of the background area for the tab
+            bg_color = '#f0f0f0',
+            -- The color of the text for the tab
+            fg_color = '#202020',
+         },
+         -- Inactive tabs are the tabs that do not have focus
+         inactive_tab = {
+            bg_color = '#c0c0c0',
+            fg_color = '#808080',
+         },
+         -- You can configure some alternate styling when the mouse pointer
+         -- moves over inactive tabs
+         inactive_tab_hover = {
+            bg_color = '#d7d7d7',
+            fg_color = '#505050',
+         },
+         -- The new tab button that let you create new tabs
+         new_tab = {
+            bg_color = '#c0c0c0',
+            fg_color = '#505050',
+         },
+         -- You can configure some alternate styling when the mouse pointer
+         -- moves over the new tab button
+         new_tab_hover = {
+            bg_color = '#d7d7d7',
+            fg_color = '#505050',
+         },
+         -- The color of the inactive tab bar edge/divider
+         inactive_tab_edge = '#f0f0f0',
+      },
+   },
+}
--- wezterm.lua
+++ wezterm.lua
@@ -1,30 +1,40 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-local keybindings = require 'keybindings'
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- ######## Global style ########
 
-config.color_scheme = 'Google Dark (Gogh)'
+config.color_scheme = 'Modus-Operandi-Tritanopia'
 
-config.window_background_opacity = 0.85
+config.window_frame = {
+   -- アクティブウィンドウの時のタイトルバー/タブバーの色
+   active_titlebar_bg = '#e0e0e0',
+   -- 非アクティブウィンドウの時のタイトルバー/タブバーの色
+   inactive_titlebar_bg = '#060011',
+}
 
 config.font = wezterm.font_with_fallback {
   {family = 'Monaco', weight = 'Regular'},
   {family = 'Noto Sans JP', weight = 'Medium'},
   {family = 'Hiragino Sans W5', weight = 'Regular'},
 }
-config.font_size = 14
 
+config.font_size = 14
 config.initial_rows = 60
 config.initial_cols = 220
 
+-- ######## Tab bar style ########
+
+local tabbarstyle = require 'tabbarstyle'
+config.colors = tabbarstyle.style
+
+-- ######## Key bindings ########
+
+local keybindings = require 'keybindings'
 config.disable_default_key_bindings = true
 config.keys = keybindings.keys
 
+-- ######## For macOS IME problem ########
+
 config.macos_forward_to_ime_modifier_mask = "CTRL|SHIFT"
 
--- and finally, return the configuration to wezterm
 return config
